d4d8be30f1f0ab56fe5e11820864ebae7548dab6
abombalemba <leaderbomjganga@mail.ru>
2025-09-20 18:45:16 +0300

    fix: upgraded init package
    included necessary dependecies; deleted demo project

--- src/src/main/java/com/cpaums/cpaums/CpaumsApplication.java
+++ src/src/main/java/com/cpaums/cpaums/CpaumsApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.cpaums.cpaums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CpaumsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CpaumsApplication.class, args);
 	}
 
 }
--- src/src/test/java/com/cpaums/cpaums/CpaumsApplicationTests.java
+++ src/src/test/java/com/cpaums/cpaums/CpaumsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.cpaums.cpaums;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CpaumsApplicationTests {
 
 	@Test
 	void contextLoads() {
